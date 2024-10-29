61e98dc7f47a426d4031579f3931f97e8aa03685
Tatiana Trofimova <nerpassevera@gmail.com>
2024-10-28 21:13:42 -0700

    Updates get one, get all  routes and creates update route

--- app/models/book.py
+++ app/models/book.py
@@ -5,3 +5,11 @@ class Book(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     description: Mapped[str]
+
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description
+        }
--- app/routes/books_routes.py
+++ app/routes/books_routes.py
@@ -1,26 +1,56 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response, request, Response
+from app.models.book import Book
+from app.db import db
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
-# @books_bp.get("/", strict_slashes=False)
-# def get_all_books():
-#     return [book.to_dict() for book in books]
+@books_bp.post("/", strict_slashes=False)
+def create_book():
+    request_body = request.get_json()
+    title = request_body["title"]
+    description = request_body["description"]
 
-# # @books_bp.get("/<int:book_id>")
-# @books_bp.get("/<book_id>")
-# def get_one_book(book_id):
-#     book = validate_book(book_id)
-#     return book
+    new_book = Book(title=title, description=description)
+    db.session.add(new_book)
+    db.session.commit()
 
-# def validate_book(book_id):
-#     try:
-#         book_id = int(book_id)
-#     except ValueError:
-#         message = {"message": f"book id {book_id} is invalid"}
-#         abort(make_response(message, 400))
+    response = new_book.to_dict()
+    return response, 201
 
-#     for book in books:
-#         if book.id == book_id:
-#             return book.to_dict()
+@books_bp.get("/", strict_slashes=False)
+def get_all_books():
+    query = db.select(Book).order_by(Book.id)
+    books = db.session.scalars(query)
+    return [book.to_dict()for book in books]
 
-#     abort(make_response({ "message": f"Book with ID {book_id} not found"}, 404))
+@books_bp.get("/<book_id>", strict_slashes=False)
+
+def get_one_book(book_id):
+    book = validate_book(book_id)
+    return book.to_dict()
+
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except ValueError:
+        message = {"message": f"book id {book_id} is invalid"}
+        abort(make_response(message, 400))
+
+    query = db.select(Book).where(Book.id == book_id)
+    book = db.session.scalar(query)
+
+    if book:
+        return book
+
+    abort(make_response({ "message": f"Book with ID {book_id} not found"}, 404))
+
+@books_bp.put("/<book_id>", strict_slashes=False)
+def edit_a_book(book_id):
+    book = validate_book(book_id)
+    request_body = request.get_json()
+
+    book.title = request_body["title"]
+    book.description = request_body["description"]
+
+    db.session.commit()
+    return Response(status=204, mimetype="application/json")
